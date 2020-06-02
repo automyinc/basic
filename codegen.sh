79ba47f925218ff18ff2e8c53f238778663decdf
Max Wittal <wittal@neobotix.de>
2020-06-02 13:03:22 +0200

    codegen update

--- codegen.sh
+++ codegen.sh
@@ -2,7 +2,18 @@
 
 VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
 
+MATH_INCLUDE=$1
+VNX_INCLUDE=$2
+
+if [ -z "$MATH_INCLUDE" ]; then
+	MATH_INCLUDE=${VNX_INTERFACE_DIR}/automy/math/
+fi
+
+if [ -z "$VNX_INCLUDE" ]; then
+	VNX_INCLUDE=${VNX_INTERFACE_DIR}/vnx/
+fi
+
 cd $(dirname "$0")
 
-vnxcppcodegen --cleanup generated/ automy.basic interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ ${VNX_INTERFACE_DIR}/automy/math/
+vnxcppcodegen --cleanup generated/ automy.basic interface/ modules/ $MATH_INCLUDE $VNX_INCLUDE
 
