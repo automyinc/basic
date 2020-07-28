e7edd211c5795d791a43b4621bd37304cf33678a
Max Wittal <wittal@neobotix.de>
2020-07-28 13:40:04 +0200

    JSON Image read support

--- include/automy/basic/Image.hpp
+++ include/automy/basic/Image.hpp
@@ -6,6 +6,7 @@
 
 #include <vnx/Input.hpp>
 #include <vnx/Output.hpp>
+#include <vnx/Object.hpp>
 
 
 namespace automy {
@@ -23,31 +24,66 @@ void Image<T>::write_to_file(const std::string& filename, int number, int digits
 
 template<typename T>
 void Image<T>::read(vnx::TypeInput& in, const vnx::TypeCode* type_code, const uint16_t* code) {
-	if(code[0] == vnx::CODE_IMAGE || code[0] == vnx::CODE_ALT_IMAGE) {		// new format since vnx-1.2.0
-		std::array<size_t, 3> size_;
-		vnx::read_image_size<3>(in, size_, code);
-		resize(size_[1], size_[2], size_[0]);
-		vnx::read_image_data<T, 3>(in, data_, size_, code);
-	} else if(code[0] == vnx::CODE_ARRAY && code[1] == 4 && code[2] == vnx::CODE_DYNAMIC) {		// old format
-		{
-			uint32_t size[3] = {0, 0, 0};
-			vnx::read_dynamic(in, size[0]);
-			vnx::read_dynamic(in, size[1]);
-			vnx::read_dynamic(in, size[2]);
-			resize(size[0], size[1], size[2]);
+	switch(code[0]) {
+		case vnx::CODE_IMAGE:
+		case vnx::CODE_ALT_IMAGE: {
+			// new format since vnx-1.2.0
+			std::array<size_t, 3> size_;
+			vnx::read_image_size<3>(in, size_, code);
+			resize(size_[1], size_[2], size_[0]);
+			vnx::read_image_data<T, 3>(in, data_, size_, code);
+			break;
 		}
-		uint16_t code[VNX_MAX_BYTE_CODE_SIZE];
-		size_t size = 0;
-		vnx::read_dynamic_list_size<T>(in, code, size);
-		if(size == get_size()) {
-			vnx::read_dynamic_list_data<T>(in, data_, code, size);
-		} else {
-			vnx::read_dynamic_list_data<T>(in, 0, code, size);
+		case vnx::CODE_OBJECT:
+		case vnx::CODE_ALT_OBJECT: {
+			// JSON format
+			vnx::Object object;
+			vnx::read(in, object, type_code, code);
+			std::array<size_t, 3> size;
+			object["size"].to(size);
+			resize(size[1], size[2], size[0]);
+			std::vector<T> data;
+			object["data"].to(data);
+			if(data.size() == get_size()) {
+				for(size_t i = 0; i < data.size(); ++i) {
+					data_[i] = data[i];
+				}
+			} else {
+				clear();
+			}
+			break;
+		}
+		case vnx::CODE_ARRAY:
+			// old format
+			if(code[1] == 4 && code[2] == vnx::CODE_DYNAMIC) {
+				{
+					uint32_t size[3] = {0, 0, 0};
+					vnx::read_dynamic(in, size[0]);
+					vnx::read_dynamic(in, size[1]);
+					vnx::read_dynamic(in, size[2]);
+					resize(size[0], size[1], size[2]);
+				}
+				uint16_t code[VNX_MAX_BYTE_CODE_SIZE];
+				size_t size = 0;
+				vnx::read_dynamic_list_size<T>(in, code, size);
+				if(size == get_size()) {
+					vnx::read_dynamic_list_data<T>(in, data_, code, size);
+				} else {
+					vnx::read_dynamic_list_data<T>(in, 0, code, size);
+					clear();
+				}
+			}
+			break;
+		case vnx::CODE_DYNAMIC:
+		case vnx::CODE_ALT_DYNAMIC: {
+			uint16_t dyn_code[VNX_MAX_BYTE_CODE_SIZE];
+			vnx::read_byte_code(in, dyn_code);
+			Image<T>::read(in, type_code, dyn_code);
+			break;
+		}
+		default:
+			vnx::skip(in, type_code, code);
 			clear();
-		}
-	} else {
-		vnx::skip(in, type_code, code);
-		clear();
 	}
 }
 
